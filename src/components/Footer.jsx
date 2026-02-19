9f11f2b97385e6d8c43734da09d4e5f09404c343
Chathuka Bimsara Hetti Arachchi <161861880+ChathukaHettiArachchi@users.noreply.github.com>
2026-02-19 09:46:10 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-8">
-      <p>© 2026 Elegant Textile. All rights reserved.</p>
-    </footer>
-  );
-}
